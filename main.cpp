a82b8f0f1b4ae37e436e82b9d60750a6c1cede38
krco253 <kelsey.cole@uky.edu>
2018-09-19 22:22:18 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,36 +1,27 @@
 /*---------------------------------------------
-| Project: Megaminx
+| Project: AStar
 | Author: Kelsey Cole
-| Description:
-| Compile with:
-| Idea: store ANSI codes in colors array/map
-| then use B's to represent individual cubies
-| Idea: split up faces_array into 2 sides
+| Description: Solve a randomized Megaminx by AStar
+| Compile with: gcc -std=c++11 -o AStar AStar.cpp
+| Megaminx.cpp Face.cpp Row.cpp Block.cpp
 |----------------------------------------------*/
 #include "AStar.h"
 
 int main()
 {
+	//allocate a Megaminx to test
 	Megaminx test;
-	Megaminx blank;
-//	if(test == blank)
-//		std::cout << "true" << std::endl;
-//	else
-//		std::cout << "false" << std::endl;
-	int moves_to_solve = 0;
-//	test.print(); //prints Megaminx
+	int moves_to_solve = 0; //this moves_to_solve variable is unused - needs taken out
+//	test.print(); //prints the initial Megaminx
 	test.scramble(); //randomizes Megaminx
-//	test.scramble_bottom(1);
 //	std::cout << std::endl << "Scrambled Megaminx: ";	
 //	std::cout << std::endl << std::endl;
-//	test.print();
-//	blank = test.rotate_top();
-//	test.rotate_bot();	
+//	test.print(); //prints the randomized Megaminx
 	AStar test_solution(test);
 	blank = test_solution.solve(moves_to_solve);
 	std::cout << "SOLVED!" << std::endl;
 	std::cout << "Nodes expanded: "<< test_solution.nodes_expanded << std::endl;
 	std::cout << "H: " << blank.h << "G: " << blank.g << "F:  " << blank.f << std::endl << std::endl;
-//	blank.print();
+//	blank.print(); //prints out the solved Megaminx for confirmation
 	return 0;
 }
